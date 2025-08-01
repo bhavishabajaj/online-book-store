d782afc48a6123def429dda549c071de57d55711
Bhavisha <bhavishabajaj20@gmail.com>
2025-08-01 14:43:32 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+//console.log("hye cutie")
+//if-else
+var age=50;
+if(age<10){
+    console.log("you cannot drink rasna")
+}
+else if(age<30){
+    console.log("you can drink rasna")
+}
+else{
+    console.log("still youu cannot drink rasna")
+}
+console.log("end of ldder")
